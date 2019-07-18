fe8c13b70a836b70c8302f2167f777084b181aa9
Bon Teerapat <bonteerapat17@gmail.com>
2019-07-19 01:43:01 +0700

    Created Exercise4_Teerapat_P.py

---
+++ Exercise4_Teerapat_P.py
@@ -0,0 +1,9 @@
+FoundEng = 82.65
+GenBus = 74.52
+IntroComSys = 80.64
+ComProg = 78.48
+print(3*"-","Your Score",3*"-")
+print("Foundation English :",FoundEng)
+print("General Business :",GenBus)
+print("Introduction to Computer System :",IntroComSys)
+print("Computer Programming :",ComProg)
